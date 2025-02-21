197fa7dfa1da39205488c8775133ff1eb967a8fe
ecyates <ecyates1@mac.com>
2025-02-20 16:44:19 -0800

    Add workflow for Vercel deployment

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -4,13 +4,15 @@ import { getAuth, Auth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore';
 import { myApiKey } from "./utilities/apiUtilities";
 
+
+
 const firebaseConfig = {
     apiKey: myApiKey,
     authDomain: "fir-ecommerce-app-4b633.firebaseapp.com",
     projectId: "fir-ecommerce-app-4b633",
     storageBucket: "fir-ecommerce-app-4b633.firebasestorage.app",
     messagingSenderId: "695649952219",
-    appId: "1:695649952219:web:4444e19db8d3b7e95207fe"
+    appId: "1:695649952219:web:4444e19db8d3b7e95207fe",
 };
 
 // Initialize Firebase
